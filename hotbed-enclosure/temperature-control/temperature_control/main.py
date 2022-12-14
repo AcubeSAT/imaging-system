a1f5d848d3dcd354f6f9f6fbb017ab1ffbc7a56a
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-12-14 03:51:58 +0200

    main: Read config using get_path

--- hotbed-enclosure/temperature-control/temperature_control/main.py
+++ hotbed-enclosure/temperature-control/temperature_control/main.py
@@ -4,9 +4,12 @@ from PySide6.QtWidgets import QApplication
 
 from GUI import TempLogUtilsGUI
 from IOUtils import read_config
+from paths import get_path
 
 if __name__ == "__main__":
-    CONFIG = read_config('config.toml')
+    RELATIVE_PATHS = True
+
+    CONFIG = read_config(get_path("config", RELATIVE_PATHS))
     WINDOW_CONFIG = CONFIG.window
 
     app = QApplication([])
