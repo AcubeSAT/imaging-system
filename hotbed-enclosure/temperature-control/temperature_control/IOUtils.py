981829091b0bf06ecd579421db1e9a02a7052b0f
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-12-14 04:56:10 +0200

    io: Read CSV

--- hotbed-enclosure/temperature-control/temperature_control/IOUtils.py
+++ hotbed-enclosure/temperature-control/temperature_control/IOUtils.py
@@ -1,8 +1,18 @@
 from pathlib import Path
 from tomllib import load
 
+from pandas import DataFrame, read_csv
+
 from config_model import Config
-from eltypes import config, config_model
+from eltypes import config, config_model, log_data
+
+
+def _read_csv(filename: Path) -> DataFrame:
+    return read_csv(filename, sep=',')
+
+
+def read_file(filename: Path) -> log_data:
+    return _read_csv(filename)
 
 
 def _read_toml(filename: Path) -> config:
--- hotbed-enclosure/temperature-control/temperature_control/eltypes.py
+++ hotbed-enclosure/temperature-control/temperature_control/eltypes.py
@@ -1,4 +1,8 @@
+from pandas import DataFrame
+
 from config_model import Config
 
 config = dict
 config_model = Config
+
+log_data = DataFrame
