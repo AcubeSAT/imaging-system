364cc74854e966f6f2565817b065339dbe67ed0d
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-12-14 03:50:36 +0200

    util: Ability to choose if path is relative or absolute

--- hotbed-enclosure/temperature-control/temperature_control/paths.py
+++ hotbed-enclosure/temperature-control/temperature_control/paths.py
@@ -5,5 +5,5 @@ from pyprojroot import here
 _PATHS = {"config": "temperature_control/config.toml"}
 
 
-def get_path(name: str) -> Path:
-    return here(_PATHS[name])
+def get_path(name: str, relative: bool) -> Path:
+    return here(_PATHS[name]) if not relative else _PATHS[name]
