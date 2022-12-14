890a4b5ad3e89f1ca27572662acc6983ddd6d15b
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-12-14 03:39:31 +0200

    io: Read TOML config file

--- hotbed-enclosure/temperature-control/temperature_control/IOUtils.py
+++ hotbed-enclosure/temperature-control/temperature_control/IOUtils.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 from tomllib import load
 
+from config_model import Config
+
 
 def _read_toml(filename: Path):
     with open(filename, mode = 'rb') as fp:
         config = load(fp)
     return config
+
+def read_config(filename: Path):
+    conf_from_file = _read_toml(filename)
+    return Config.parse_obj(conf_from_file)
