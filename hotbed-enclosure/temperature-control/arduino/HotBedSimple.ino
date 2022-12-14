afc46805df276d528870a3c6536a14bc3094a0b2
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-12-14 20:26:36 +0200

    arduino: Add dummy hotbed control

---
+++ hotbed-enclosure/temperature-control/arduino/HotBedSimple.ino
@@ -0,0 +1,18 @@
+#include "DHT.h"
+#define DHTPIN 4    
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  pinMode(7,OUTPUT);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+  float t = dht.readTemperature();
+  Serial.print(t);
+  if(t<=31) digitalWrite(7,HIGH);
+  if(t>31) digitalWrite(7,LOW);
+}
